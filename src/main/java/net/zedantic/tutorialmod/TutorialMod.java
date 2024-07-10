7d708922dd8a2251d331c838b344dab3be48c998
Harrison <fishopha@gmail.com>
2024-07-10 13:58:31 -0300

    Sprinkler and Umbrella item

--- src/main/java/net/zedantic/tutorialmod/TutorialMod.java
+++ src/main/java/net/zedantic/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.zedantic.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.zedantic.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,5 +13,7 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world! Its me!");
+
+		ModItems.registerModItems();
 	}
 }
